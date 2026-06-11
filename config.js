f48666bad1f0f3e204611968cb42854469fb6864
golamhukkani <140696565+golamhukkani@users.noreply.github.com>
2026-06-11 15:17:02 +0600

    Delete config.js

--- config.js
+++
@@ -1,2 +0,0 @@
-// 1) Paste your deployed Google Apps Script Web App URL here.
-window.API_URL = "https://script.google.com/macros/s/AKfycbzAlDhMzLmyuUrBVhnvUTce-kPsn_dtOWqygh9NXC8n1mv8S9qYUW-pTSq8EIoXvaGT/exec";
